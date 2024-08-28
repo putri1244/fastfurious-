8073b40210bb24597c3e8522e11867ed8742b8fb
putri1244 <119003234+putri1244@users.noreply.github.com>
2024-08-29 05:44:22 +0700

    update

--- website/js/script.js
+++ website/js/script.js
@@ -1,5 +1,13 @@
-class ijn {
-    constructor(parameters) {
-        
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    
+    if (username === 'admin' && password === 'password') {
+        alert('Login sukses!');
+        window.location.href = 'dashboard.html'; // Redirect ke halaman utama
+    } else {
+        document.getElementById('errorMessage').innerText = 'Username atau Password salah!';
     }
-}
+});
